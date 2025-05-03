cd0427de51e45300b740fdf754827833b60fe27f
rahat728 <asifuzzamanrahat728@gmail.com>
2025-05-03 22:25:06 +0600

    authenticaton done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,24 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import IndexPage from "./pages/IndexPage.jsx";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import Layout from "./Layout";
+import ProfilePage from "./pages/ProfilePage";
+import {UserContextProvider} from "./UserContext";
 
 const App = () => {
   return (
+    <UserContextProvider>
     <Routes>
       <Route path="/" element={<Layout />}>
          <Route path="/login" element={<LoginPage />} />
          <Route index element={<IndexPage />} />
+         <Route path="/register" element={<RegisterPage />} />
+         <Route path="/account" element={<ProfilePage />} />
       </Route>
 
-  </Routes>
+    </Routes>
+  </UserContextProvider>
   )
 }
 
